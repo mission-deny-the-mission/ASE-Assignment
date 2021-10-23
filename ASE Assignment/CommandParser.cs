ca77e4b21df177f1cb2229479efed4abb06fbca9
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2021-10-23 02:18:26 +0100

    Change approach to command processing

--- ASE Assignment/CommandParser.cs
+++ ASE Assignment/CommandParser.cs
@@ -9,32 +9,68 @@ namespace ASE_Assignment
 {
     class CommandParser
     {
-        enum command
-        {
-            While, Var, Draw, Method, If
-        }
 
         IDictionary<string, int> Variables = new Dictionary<string, int>();
-        public CommandParser()
+        public CommandParser(string script)
         {
-
+            foreach (string line in script.Split('\n'))
+            {
+                executeLine(line);
+            }
+        }
+        struct valueSymbol
+        {
+            public bool isVariable;
+            public string? name;
+            public int value;
+        }
+        private valueSymbol decodeValueSymbol(string symbol)
+        {
+            valueSymbol result = new valueSymbol();
+            if (Char.IsLetter(symbol[0]))
+            {
+                if (Variables.ContainsKey(symbol))
+                {
+                    result.name = symbol;
+                    result.isVariable = true;
+                    result.value = Variables[symbol];
+                    return result;
+                } else
+                {
+                    throw new Exception("Variable is not valid");
+                }
+            } else
+            {
+                if (int.TryParse(symbol, out int value))
+                {
+                    result.value = value;
+                    result.isVariable = false;
+                    return result;
+                } else
+                {
+                    throw new Exception("Symbol is not a valid number or variable");
+                }
+            }
         }
         public void executeLine(string line)
         {
-            
-        }
+            string[] words = new string[];
+            foreach (string word in line.Split(' ')
+            {
+                words.Append(word);
+            }
 
-        public void executeScript(string script)
-        {
-
-        }
-
-        private command commandDecode(string command)
-        {
+            string command = words[0];
             switch (command)
             {
                 case "While":
-                    return CommandParser.command.While;
+                    if (words.Length == 4)
+                    {
+
+                    } else
+                    {
+                        throw new Exception("While does not have the correct number of arguments supplied.");
+                    }
                     break;
                 case "Method":
                     return CommandParser.command.Method;
@@ -47,8 +83,17 @@ namespace ASE_Assignment
                 default:
                     throw new Exception("There is something wrong with the command");
             }
+
+            
         }
 
+        public void executeScript(string script)
+        {
+            foreach (string line in script.Split('\n'))
+            {
+                executeLine(line);
+            }
+        }
         public void Graphics_Paint(object sender, System.Windows.Forms.PaintEventArgs pe)
         {
             System.Drawing.Graphics g = pe.Graphics;
